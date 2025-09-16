d5ebcbdb52b58b0a7be31795ea6f69f37a1df748
zapsmart <zapsmart0@gmail.com>
2025-09-16 12:11:09 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( )],
+  base: './', // <--- Esta linha foi adicionada/modificada
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
