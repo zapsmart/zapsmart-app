fdc21a68c80682b1b8ca924c31a809a5cefc6d20
zapsmart <zapsmart0@gmail.com>
2025-09-19 16:14:04 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Remova ou comente o código que registra o Service Worker
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js').then(
+//       (registration) => {
+//         console.log('Service Worker registered: ', registration);
+//       },
+//       (err) => {
+//         console.log('Service Worker registration failed: ', err);
+//       }
+//     );
+//   });
+// }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
